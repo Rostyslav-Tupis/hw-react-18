7aa5cf80d059cc5019eba10d90fad64a4a1ac504
Rostyslavichh <rostyslavichh@gmail.com>
2023-02-17 23:05:41 +0200

    added a animation to timer

--- src/timer component/timer.js
+++ src/timer component/timer.js
@@ -2,18 +2,26 @@ import React , {useState, useEffect } from "react";
 import './style.css'
 
 function Timer({step , timerTime, autoStart}){
+    let originalWidthOfLine = 100
     const [time , setTime] = useState(timerTime)
     const [isRunning , setStatusBtn] = useState(autoStart)
+    let [lineWidth , setWidth] = useState(originalWidthOfLine)
 
     function formatTime(seconds) { //ділим на 60 щоб получити хв , тоді добавляємо спереді 0 та slice останні два елемента
         return `${("0" + Math.floor(seconds / 60)).slice(-2)} : ${("0" + seconds % 60).slice(-2)}`
     }
 
+    
+    let oneSecPercent = lineWidth / timerTime //скільки одна секунда займає відсотків !!
+
     useEffect(()=>{
         let interval;
         if(isRunning && time > 0 ){
 
+            setWidth(lineWidth -= oneSecPercent)
+
             console.log(`${formatTime(time)} Часу залишилося`);
+
             interval = setInterval(()=>{
                 setTime((prev)=> {
                     return prev - 1 
@@ -47,6 +55,7 @@ function Timer({step , timerTime, autoStart}){
 
     function handleReset(){
         setTime(timerTime)
+        setWidth(originalWidthOfLine)
     }
 
     return(
@@ -58,6 +67,9 @@ function Timer({step , timerTime, autoStart}){
                         {isRunning ? "Stop" : "Start"}
                     </button>
                     <button onClick={handleReset}>Reset</button>
+                </div>
+                <div className="wrapper">
+                    <div className="timeLine" style={{width: lineWidth + "%"}}></div>
                 </div>
             </div>
         </div>
